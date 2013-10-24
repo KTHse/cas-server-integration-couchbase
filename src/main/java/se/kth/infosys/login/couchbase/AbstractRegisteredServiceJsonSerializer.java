14fcc1d4584409ed87a256bac21da5faff16b598
Fredrik Jönsson <fjo@kth.se>
2013-10-24 17:01:57 +0200

    Fix all sorts of checkstyle pains.

--- src/main/java/se/kth/infosys/login/couchbase/AbstractRegisteredServiceJsonSerializer.java
+++ src/main/java/se/kth/infosys/login/couchbase/AbstractRegisteredServiceJsonSerializer.java
@@ -1,21 +1,21 @@
 package se.kth.infosys.login.couchbase;
 
 /*
-   Copyright (C) 2013 KTH, Kungliga tekniska hogskolan, http://www.kth.se
-
-   This file is part of cas-server-integration-couchbase.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+ * Copyright (C) 2013 KTH, Kungliga tekniska hogskolan, http://www.kth.se
+ *
+ * This file is part of cas-server-integration-couchbase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import java.lang.reflect.Type;
@@ -33,32 +33,46 @@ import com.google.gson.JsonSerializer;
 
 
 /**
- * Helper class for serializing derivatives of the abstract class 
+ * Helper class for serializing derivatives of the abstract class
  * AbstractRegisteredService in a way that allows us to re-instantiate
  * the the object using the proper class. It will store the object "type"
  * of the class in the JSON object together with a "properties" field
  * containing the values of the object properties.
  */
-public class AbstractRegisteredServiceJsonSerializer 
+public class AbstractRegisteredServiceJsonSerializer
 implements JsonSerializer<AbstractRegisteredService>, JsonDeserializer<AbstractRegisteredService> {
 
-	public JsonElement serialize(AbstractRegisteredService src, 
-			Type typeOfSrc, JsonSerializationContext context) {
-		JsonObject result = new JsonObject();
-		result.add("type", new JsonPrimitive(src.getClass().getName()));
-		result.add("properties", context.serialize(src, src.getClass()));
-		return result;
-	}
+    /**
+     * @param src CAS Service.
+     * @param typeOfSrc (ignored).
+     * @param context Gson serialization context.
+     * @see JsonSerializer#serialize(Object, Type, JsonSerializationContext)
+     * @return JSON serialized version of a CAS service.
+     */
+    public final JsonElement serialize(
+            final AbstractRegisteredService src, final Type typeOfSrc, final JsonSerializationContext context) {
+        JsonObject result = new JsonObject();
+        result.add("type", new JsonPrimitive(src.getClass().getName()));
+        result.add("properties", context.serialize(src, src.getClass()));
+        return result;
+    }
 
-	public AbstractRegisteredService deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-			throws JsonParseException {
-		JsonObject jsonObject = json.getAsJsonObject();
-		String type = jsonObject.get("type").getAsString();
-		JsonElement element = jsonObject.get("properties");
-		try {
-			return context.deserialize(element, Class.forName(type));
-		} catch (ClassNotFoundException e) {
-			throw new JsonParseException("Unknown element type: " + type, e);
-		}
-	}
+    /**
+     * @see JsonDeserializer#deserialize(JsonElement, Type, JsonDeserializationContext)
+     * @param json JSON encoded representation of CAS Service.
+     * @param typeOfT (ignored).
+     * @param context Gson de-serialization context.
+     * @return de-serialized version of a CAS service.
+     */
+    public final AbstractRegisteredService deserialize(
+            final JsonElement json, final Type typeOfT, final JsonDeserializationContext context) {
+        JsonObject jsonObject = json.getAsJsonObject();
+        String type = jsonObject.get("type").getAsString();
+        JsonElement element = jsonObject.get("properties");
+        try {
+            return context.deserialize(element, Class.forName(type));
+        } catch (final ClassNotFoundException e) {
+            throw new JsonParseException("Unknown element type: " + type, e);
+        }
+    }
 }
--- src/main/java/se/kth/infosys/login/couchbase/CouchbaseClientFactory.java
+++ src/main/java/se/kth/infosys/login/couchbase/CouchbaseClientFactory.java
@@ -1,21 +1,21 @@
 package se.kth.infosys.login.couchbase;
 
 /*
-   Copyright (C) 2013 KTH, Kungliga tekniska hogskolan, http://www.kth.se
-
-   This file is part of cas-server-integration-couchbase.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+ * Copyright (C) 2013 KTH, Kungliga tekniska hogskolan, http://www.kth.se
+ *
+ * This file is part of cas-server-integration-couchbase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import java.net.URI;
@@ -43,164 +43,174 @@ import com.couchbase.client.protocol.views.ViewDesign;
  * until successful connection is made.
  */
 public class CouchbaseClientFactory extends TimerTask {
-	private static final Logger logger = LoggerFactory.getLogger(CouchbaseClientFactory.class);
-	private static final int RETRY_INTERVAL = 10; // seconds.
-	private final Timer timer = new Timer(); 
+    private final Logger logger = LoggerFactory.getLogger(CouchbaseClientFactory.class);
+    private static final int RETRY_INTERVAL = 10; // seconds.
+    private final Timer timer = new Timer();
 
-	private CouchbaseClient client;
+    private CouchbaseClient client;
 
-	@NotNull
-	private List<URI> uris;
+    @NotNull
+    private List<URI> uris;
 
-	/* The name of the bucket, will use the default bucket unless otherwise specified. */
-	private String bucket = "default";
+    /* The name of the bucket, will use the default bucket unless otherwise specified. */
+    private String bucket = "default";
 
-	/* NOTE: username is currently not used in Couchbase 2.0, may be in the future. */
-	private String username = "";
+    /* NOTE: username is currently not used in Couchbase 2.0, may be in the future. */
+    private String username = "";
 
-	/* Password for the bucket if any. */
-	private String password = "";
+    /* Password for the bucket if any. */
+    private String password = "";
 
-	/* Design document and views to create in the bucket, if any. */
-	private String designDocument;
-	private List<ViewDesign> views;
-
-
-	/**
-	 * Default constructor. 
-	 */
-	public CouchbaseClientFactory() {}
+    /* Design document and views to create in the bucket, if any. */
+    private String designDocument;
+    private List<ViewDesign> views;
 
 
-	/**
-	 * Start initializing the client. This will schedule a task that retries 
-	 * connection until successful.
-	 */
-	public void initialize() {
-		timer.scheduleAtFixedRate(this, new Date(), TimeUnit.SECONDS.toMillis(RETRY_INTERVAL));
-	}
+    /**
+     * Default constructor. 
+     */
+    public CouchbaseClientFactory() {}
 
 
-	/**
-	 * Inverse of initialize, shuts down the client, cancelling connection 
-	 * task if not completed.
-	 * 
-	 * @throws Exception on errors.
-	 */
-	public void shutdown() throws Exception {
-		timer.cancel();
-		timer.purge();
-		if (client != null) {
-			client.shutdown();
-		}
-	}
+    /**
+     * Start initializing the client. This will schedule a task that retries
+     * connection until successful.
+     */
+    public void initialize() {
+        timer.scheduleAtFixedRate(this, new Date(), TimeUnit.SECONDS.toMillis(RETRY_INTERVAL));
+    }
 
 
-	/**
-	 * Fetch a client for the database.
-	 * 
-	 * @return the client if available.
-	 * @throws RuntimeException if client is not initialized yet.
-	 */
-	public CouchbaseClient getClient() {
-		if (client != null) {
-			return client;
-		} else {
-			throw new RuntimeException("Conncetion to bucket " + bucket + " not initialized yet.");
-		}
-	}
+    /**
+     * Inverse of initialize, shuts down the client, cancelling connection
+     * task if not completed.
+     * 
+     * @throws Exception on errors.
+     */
+    public void shutdown() throws Exception {
+        timer.cancel();
+        timer.purge();
+        if (client != null) {
+            client.shutdown();
+        }
+    }
 
 
-	/**
-	 * Register indexes to ensure in the bucket when the client is initialized.
-	 * 
-	 * @param documentName name of the Couchbase design document.
-	 * @param views the list of Couchbase views (i.e. indexes) to create in the document.
-	 */
-	public void ensureIndexes(String documentName, List<ViewDesign> views) {
-		this.designDocument = documentName;
-		this.views = views;
-	}
+    /**
+     * Fetch a client for the database.
+     * 
+     * @return the client if available.
+     * @throws RuntimeException if client is not initialized yet.
+     */
+    public CouchbaseClient getClient() {
+        if (client != null) {
+            return client;
+        } else {
+            throw new RuntimeException("Conncetion to bucket " + bucket + " not initialized yet.");
+        }
+    }
 
 
-	/**
-	 * Ensures that all views exists in the database.
-	 * 
-	 * @param documentName the name of the design document.
-	 * @param views the views to ensure exists in the database.
-	 */
-	@SuppressWarnings("unchecked")
-	private void doEnsureIndexes(String documentName, List<ViewDesign> views) {
-		DesignDocument<ViewDesign> document;
-		try {
-			document = client.getDesignDocument(documentName);
-			List<ViewDesign> oldViews = document.getViews();
-
-			for (ViewDesign view : views) {
-				if (!isViewInList(view, oldViews)) {
-					throw new InvalidViewException("Missing view: " + view.getName());
-				}
-			}
-			logger.info("All views are already created for bucket " + bucket);
-		} catch (InvalidViewException e) {
-			logger.warn("Missing indexes in database for document " + documentName + ", creating new.");
-			document = new DesignDocument<ViewDesign>(documentName);
-			for (ViewDesign view : views) {
-				document.getViews().add(view);
-				if (! client.createDesignDoc(document)) {
-					throw new InvalidViewException("Failed to create views.");
-				}
-			}
-		}
-	}
+    /**
+     * Register indexes to ensure in the bucket when the client is initialized.
+     * 
+     * @param documentName name of the Couchbase design document.
+     * @param views the list of Couchbase views (i.e. indexes) to create in the document.
+     */
+    public void ensureIndexes(final String documentName, final List<ViewDesign> views) {
+        this.designDocument = documentName;
+        this.views = views;
+    }
 
 
-	private static boolean isViewInList(ViewDesign needle, List<ViewDesign> stack) {
-		for (ViewDesign view : stack) {
-			if (equals(needle, view)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * Ensures that all views exists in the database.
+     * 
+     * @param documentName the name of the design document.
+     * @param views the views to ensure exists in the database.
+     */
+    @SuppressWarnings("unchecked")
+    private void doEnsureIndexes(final String documentName, final List<ViewDesign> views) {
+        DesignDocument<ViewDesign> document;
+        try {
+            document = client.getDesignDocument(documentName);
+            List<ViewDesign> oldViews = document.getViews();
+
+            for (ViewDesign view : views) {
+                if (!isViewInList(view, oldViews)) {
+                    throw new InvalidViewException("Missing view: " + view.getName());
+                }
+            }
+            logger.info("All views are already created for bucket " + bucket);
+        } catch (final InvalidViewException e) {
+            logger.warn("Missing indexes in database for document " + documentName + ", creating new.");
+            document = new DesignDocument<ViewDesign>(documentName);
+            for (ViewDesign view : views) {
+                document.getViews().add(view);
+                if (!client.createDesignDoc(document)) {
+                    throw new InvalidViewException("Failed to create views.");
+                }
+            }
+        }
+    }
 
 
-	private static boolean equals(ViewDesign d1, ViewDesign d2) {
-		return (d1.getName().equals(d2.getName())
-				&& d1.getMap().equals(d2.getMap())
-				&& d1.getReduce().equals(d2.getReduce()));
-	}
+    /**
+     * @param needle the view design to look for
+     * @param stack the list of view designs to look in
+     * @return true if needle exists in stack
+     */
+    private static boolean isViewInList(final ViewDesign needle, final List<ViewDesign> stack) {
+        for (ViewDesign view : stack) {
+            if (equals(needle, view)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
-	/**
-	 * Task to initialize the Couchbase client.
-	 */
-	public void run() {
-		try {
-			logger.info("Trying to connect to couchbase bucket " + bucket);
-			client = new CouchbaseClient(uris, bucket, username, password);
-			timer.cancel();
-			if (views != null) {
-				doEnsureIndexes(designDocument, views);
-			}
-		} catch (Exception e) {
-			logger.error("Failed to connect to Couchbase bucket " + bucket + ", retrying...");
-		}
-	}
+    /**
+     * @param d1 a view design.
+     * @param d2 another view design.
+     * @return true if designs are equal.
+     */
+    private static boolean equals(final ViewDesign d1, final ViewDesign d2) {
+        return (d1.getName().equals(d2.getName())
+                && d1.getMap().equals(d2.getMap())
+                && d1.getReduce().equals(d2.getReduce()));
+    }
 
-	public void setUris(final List<URI> uris) {
-		this.uris = uris;
-	}
 
-	public void setBucket(final String bucket) {
-		this.bucket = bucket;
-	}
+    /**
+     * Task to initialize the Couchbase client.
+     */
+    public void run() {
+        try {
+            logger.info("Trying to connect to couchbase bucket " + bucket);
+            client = new CouchbaseClient(uris, bucket, username, password);
+            timer.cancel();
+            if (views != null) {
+                doEnsureIndexes(designDocument, views);
+            }
+        } catch (final Exception e) {
+            logger.error("Failed to connect to Couchbase bucket " + bucket + ", retrying...");
+        }
+    }
 
-	public void setUsername(final String username) {
-		this.username = username;
-	}
+    public void setUris(final List<URI> uris) {
+        this.uris = uris;
+    }
 
-	public void setPassword(final String password) {
-		this.password = password;
-	}
+    public void setBucket(final String bucket) {
+        this.bucket = bucket;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
 }
